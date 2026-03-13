04c155bd9c81629b84181b3a2721ac6b6cb1dc7d
Dave Mateer <davemateer@gmail.com>
2026-03-13 09:30:07 +0000

    webserver update instructions how to build

--- infra/create_webserver.sh
+++ infra/create_webserver.sh
@@ -1,33 +1,34 @@
-#  DM created on 17th Dec 2025
-# https://docs.bayanat.org/en/install
+#  DM created on 17th Dec 2025 and updated on 12th March 2026
+# https://docs.bayanat.org/deployment/installation.html
 
 # there was duplicate python3-pip in the original list
 # don't need python3.12
 
 # don't see why I need venv etc and pip as will be using uv
 # git is there too.
-# TODO - do again (revert from backup).. after I get a working build first
-sudo apt install \
-    build-essential \
-    python3.12-dev \
-    python3.12-venv \
-    python3-pip \
-    libjpeg8-dev \
-    libzip-dev \
-    libxml2-dev \
-    libssl-dev \
-    libffi-dev \
-    libxslt1-dev \
-    libmysqlclient-dev \
-    libncurses5-dev \
-    postgresql \
-    postgresql-contrib \
-    libpq-dev \
-    git \
-    libimage-exiftool-perl \
-    postgis \
-    ffmpeg \
-    redis-server
+# sudo apt install \
+#     build-essential \
+#     python3.12-dev \
+#     python3.12-venv \
+#     python3-pip \
+#     libjpeg8-dev \
+#     libzip-dev \
+#     libxml2-dev \
+#     libssl-dev \
+#     libffi-dev \
+#     libxslt1-dev \
+#     libmysqlclient-dev \
+#     libncurses5-dev \
+#     postgresql \
+#     postgresql-contrib \
+#     libpq-dev \
+#     git \
+#     libimage-exiftool-perl \
+#     postgis \
+#     ffmpeg \
+#     redis-server
+
+sudo apt install -y python3-dev libpq-dev redis-server postgresql postgresql-contrib postgis libgdal-dev uwsgi
 
 
 # https://tesseract-ocr.github.io/tessdoc/Installation.html
@@ -35,27 +36,38 @@ sudo apt install tesseract-ocr -y
 
 # english installed by default
 # sudo apt install tesseract-ocr-eng -y
-sudo apt install tesseract-ocr-ara -y
+# arabic I think
+# sudo apt install tesseract-ocr-ara -y
 
 sudo apt install nginx -y
 
 # create non privileged user to run bayanat
-sudo useradd -m bayanat -s /bin/bash
+# sudo useradd -m bayanat -s /bin/bash
+
+# create system user without a password login ie cannot login using a password
+# common for running services
+sudo adduser --disabled-password bayanat
+
 
 # Next, Postgres user with the same name should be created, along with the bayanat database and relevant extensions on the database:
 sudo -u postgres createuser -d bayanat
-sudo -u bayanat createdb bayanat
-sudo -u postgres psql -d bayanat -c 'CREATE EXTENSION if not exists pg_trgm; CREATE EXTENSION if not exists postgis;'
+# create db and assign ownership
+sudo -u postgres createdb -O bayanat bayanat
+
+sudo -u postgres psql -d bayanat -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+sudo -u postgres psql -d bayanat -c "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
 
 
 sudo mkdir /bayanat/
-sudo chown bayanat:bayanat /bayanat/
+sudo chown bayanat:bayanat /bayanat
 
-# switch to non priv use bayanat to continue installation
-sudo su -l bayanat
+# exiftool needed before uv sync (pyexifinfo checks for it during build)
+sudo apt-get install -y libimage-exiftool-perl
 
-
-git clone https://github.com/sjacorg/bayanat.git /bayanat/
+sudo -u bayanat -i
+cd /bayanat
+# git clone https://github.com/sjacorg/bayanat.git /bayanat/
+git clone https://github.com/djhmateer/bayanat.git /bayanat/
 
 # install uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
@@ -64,27 +76,28 @@ source $HOME/.local/bin/env
 
 cd /bayanat
 
-# do I really need to do this? install perhaps better?
-uv venv
 uv sync
 
-# big dependencies.. enable for voice transscipriotns through OpenAI Whisper and OCR though Google Tesseract
+# big dependencies ie a few GB.. enable for voice transcriptions through OpenAI Whisper and OCR though Google Tesseract
 uv sync --extra ai
 
 # configure bayanat system
 # n - native install
 # just creates .env file from .env-sample
-./gen-env.sh
+bash gen-env.sh
 
-# I had to put in SECURE_COOKIES=False
+# HERE said to run uv run flask install.. but failed
+uv run flask create-db  
+uv run flask install
 
-export FLASK_APP=run.py
-uv run flask create-db
+# spins up on http://127.0.0.1:5000
+uv run flask run
 
-# I had to specify the host as running on remote test server
-# uv run flask run
+# I had to put in SECURE_COOKIES=False into the .env so didn't get error about csrf tokens not matching
 
-# specify admin user
+# as running on remote server had to do this rather than 127.0.0.1
+# remember to be as user bayanat if not already
+sudo -u bayanat -i
 uv run flask run --host=0.0.0.0
 
 # http://192.168.1.179:91/setup_wizard
